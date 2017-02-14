f1cbca97bb6135d4fa43940bc4c8e9d293f06179
Sean Riha <swriha@gmail.com>
2017-02-13 18:42:46 -0600

    Initial commit for retrying this assignment

--- main.py
+++ main.py
@@ -16,9 +16,52 @@
 #
 import webapp2
 
+username_error_text = ''
+password_error_text = ''
+verify_error_text = ''
+email_error_text = ''
+
+def build_page(username_entry, password_entry, verify_entry, email_entry):
+    username_label = "<label>Username:</label>"
+    username_input = "<input name='username' type='text' value='%s' required/>"
+    username_error = "<span class=error>%s</span>"
+
+    password_label = "<label>Password:</label>"
+    password_input = "<input name='password' type='password' required/>"
+    password_error = "<span class=error>%s</span>"
+
+    verify_label = "<label>Verify Password:</label>"
+    verify_input = "<input name='verify' type='password' required/>"
+    verify_error = "<span class=error>%s</span>"
+
+    email_label = "<label>E-mail (optional):"
+    email_input = "<input name='email' type='email' value='%s'/>"
+    email_error = "<span class=error>%s</span>"
+
+    submit = "<input type='submit'/>"
+    form = ("<form method='post'>" +
+        ("&nbsp")*11 + username_label + (username_input % username_entry) + (username_error % '') + ("<br>")*2 +
+        ("&nbsp")*12 + password_label + password_input + (password_error % password_error_text) + ("<br>")*2 +
+        "&nbsp" + verify_label + verify_input + (verify_error % verify_error_text) + ("<br>")*2 +
+        email_label + (email_input % email_entry) + (email_error % email_error_text) + ("<br>")*2 +
+        submit + "</form>")
+    header = "<h2>User Signup</h2>"
+
+    return header + form
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        content = build_page("", "", "", "")
+        self.response.write(content)
+
+    def post(self):
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
+        user_entries = build_page(username, password, verify, email)
+        self.response.write(user_entries)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
---
+++ signup-errors.py
@@ -0,0 +1,20 @@
+def error_check(username, password, verify, email=''):
+    username_error_text = ''
+    password_error_text = ''
+    verify_error_text = ''
+    email_error_text = ''
+
+    for char in username:
+        if char not 
+
+
+def username_check(username):
+
+
+def password_check(password):
+
+
+def verify_check(verify):
+
+
+def email_check(verify):
